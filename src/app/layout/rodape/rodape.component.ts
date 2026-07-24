edc3620290dbaa151490aa85d61ffd22663a4b8a
skitvandarken <skitvandarken2013@gmail.com>
2026-07-24 08:03:35 +0000

    copyright year automated

--- src/app/layout/rodape/rodape.component.ts
+++ src/app/layout/rodape/rodape.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import {  TranslateService } from '@ngx-translate/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-rodape',
-  imports: [ TranslatePipe, CommonModule, RouterLink],
+  imports: [  CommonModule, RouterLink],
   templateUrl: './rodape.component.html',
   styleUrl: './rodape.component.css'
 })
 export class RodapeComponent {
+
+  year: number = new Date().getFullYear();
 
       useLanguage(language: string): void {
           this.translate.use(language);
